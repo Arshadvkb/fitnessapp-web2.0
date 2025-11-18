d30f1fd058ffb2d81b42639bd144e065f2313abd
arshadvkb <arshadvkb118@gmail.com>
2025-11-18 16:40:37 +0530

    .

--- src/components/user/Usernavbar.tsx
+++ src/components/user/Usernavbar.tsx
@@ -5,7 +5,7 @@ const Usernavbar = () => {
   const { logout } = authStore();
   return (
     <div>
-      <nav className="min-w-screen  h-9 pt-2 flex justify-between fixed">
+      <nav className="min-w-screen  h-9 pt-2 pr-5 flex justify-between fixed">
         <div>
           <h1 className="pl-2"> GYM</h1>
         </div>
